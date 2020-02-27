ec3ce1110946f565c0fc6b4e8f51dac1f8db474b
soniagitao <soniagita.26@gmail.com>
2020-02-27 17:18:51 +0700

    Register,profile

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,64 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Checked;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.util.List;
+
+public class ProfileActivity extends AppCompatActivity  {
+    private TextView name;
+    private TextView email;
+    private TextView password;
+    private TextView confirm;
+    private TextView home;
+    private TextView about;
+    private Button visit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        name = findViewById(R.id.text_fullname);
+        email = findViewById(R.id.text_email);
+        password = findViewById(R.id.text_password);
+        confirm = findViewById(R.id.text_confirm_password);
+        home = findViewById(R.id.text_homepage);
+        about = findViewById(R.id.text_about);
+        visit = findViewById(R.id.button_homepage);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            String fullnameText = extras.getString(RegisterActivity.NAME_KEY);
+            name.setText(fullnameText);
+            String emailText = extras.getString(RegisterActivity.EMAIL_KEY);
+            email.setText(emailText);
+            String passwordText = extras.getString(RegisterActivity.PASSWORD_KEY);
+            password.setText(passwordText);
+            String confirmPassword = extras.getString(RegisterActivity.CONFIRM_KEY);
+            confirm.setText(confirmPassword);
+            String homeText = extras.getString(RegisterActivity.HOME_KEY);
+            home.setText(homeText);
+            String aboutText = extras.getString(RegisterActivity.ABOUT_KEY);
+            about.setText(aboutText);
+        }
     }
+
+
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,14 +1,154 @@
 package id.ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.util.List;
+
+public class RegisterActivity extends AppCompatActivity implements Validator.ValidationListener {
+    public static final String NAME_KEY = "";
+    public static final String EMAIL_KEY = "";
+    public static final String PASSWORD_KEY ="";
+    public static final String CONFIRM_KEY = "";
+    public static final String HOME_KEY = "";
+    public static final String ABOUT_KEY = "";
+
+    private static final int GALLERY_REQUEST_CODE = 1;
+    private static final String TAG = RegisterActivity.class.getCanonicalName();
+
+    private Button okButton;
+    private ImageView avatarImage;
+
+    @NotEmpty
+    private EditText nameEditText;
+
+    @NotEmpty
+    @Email
+    private EditText emailEditText;
+
+    @NotEmpty
+    @Password
+    private EditText passwordEditText;
+
+    @NotEmpty
+    @ConfirmPassword
+    private EditText confirmPasswordEditText;
+
+    @NotEmpty
+    private EditText homePageEditText;
+
+    @NotEmpty
+    private EditText aboutEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        nameEditText = findViewById(R.id.text_fullname);
+        emailEditText = findViewById(R.id.text_email);
+        passwordEditText = findViewById(R.id.text_password);
+        confirmPasswordEditText = findViewById(R.id.text_confirm_password);
+        homePageEditText = findViewById(R.id.text_homepage);
+        aboutEditText = findViewById(R.id.text_about);
+        okButton = findViewById(R.id.button_ok);
+
+        Validator validator = new Validator(this);
+        validator.setValidationListener(this);
+
     }
+
+
+    @Override
+    public void onValidationSucceeded() {
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm = confirmPasswordEditText.getText().toString();
+                String home = homePageEditText.getText().toString();
+                String about = aboutEditText.getText().toString();
+
+                Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
+
+                intent.putExtra(NAME_KEY,name);
+                intent.putExtra(EMAIL_KEY,email);
+                intent.putExtra(PASSWORD_KEY,password);
+                intent.putExtra(CONFIRM_KEY,confirm);
+                intent.putExtra(HOME_KEY,home);
+                intent.putExtra(ABOUT_KEY,about);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (data != null) {
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    avatarImage.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+
+    }
+
+    public void handleChangeAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
+    }
+
+
 }
